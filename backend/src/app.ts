96a09ad4c606eeeb2222d01a56dce2ff30039b5b
Vasyl Pryimak <priymak17ukrvaha@gmail.com>
2026-02-10 13:37:35 +0200

    fixed bug with cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,14 @@ import { corsOptions } from "./config/cors";
 const app = express();
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    cors(corsOptions)(req, res, next);
+  } else {
+    next();
+  }
+});
 
 app.use(express.json());
 
