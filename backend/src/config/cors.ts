b1be8a6e937bd7c2c1a72da106662e29d0514a26
Vasyl Pryimak <priymak17ukrvaha@gmail.com>
2026-02-11 20:09:16 +0200

    restore prev cors

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,18 +1,7 @@
 import { CorsOptions } from "cors";
 import { env } from "process";
 
-const allowedOrigins = [
-  env.CLIENT_URL,
-  env.DEV_CLIENT_URL,
-];
-
 export const corsOptions: CorsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: env.CLIENT_URL,
   credentials: true,
 };
