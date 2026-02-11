36191f3bc506dc1394c284f7689d6dcb1f7cc827
Vasyl Pryimak <priymak17ukrvaha@gmail.com>
2026-02-11 19:57:33 +0200

    add cor for my localhost

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,7 +1,18 @@
 import { CorsOptions } from "cors";
 import { env } from "process";
 
+const allowedOrigins = [
+  env.CLIENT_URL,
+  env.DEV_CLIENT_URL,
+];
+
 export const corsOptions: CorsOptions = {
-  origin: env.CLIENT_URL,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 };
