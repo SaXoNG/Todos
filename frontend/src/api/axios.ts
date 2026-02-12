89a2781edcb76a36883f45b2afc6b4a11c3567e8
Vasyl Pryimak <priymak17ukrvaha@gmail.com>
2026-02-12 02:07:39 +0200

    final result

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// export const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
 export const api = axios.create({
-  baseURL: "https://todos-igkf.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
